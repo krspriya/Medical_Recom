46b387de9d96687953cf43a05c24206f426eb33b
Smriti Priya <98299075+krspriya@users.noreply.github.com>
2024-06-11 19:24:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,11 +50,11 @@ symptoms_dict = {
 diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic cholestasis', 14: 'Drug Reaction', 33: 'Peptic ulcer diseae', 1: 'AIDS', 12: 'Diabetes ', 17: 'Gastroenteritis', 6: 'Bronchial Asthma', 23: 'Hypertension ', 30: 'Migraine', 7: 'Cervical spondylosis', 32: 'Paralysis (brain hemorrhage)', 28: 'Jaundice', 29: 'Malaria', 8: 'Chicken pox', 11: 'Dengue', 37: 'Typhoid', 40: 'hepatitis A', 19: 'Hepatitis B', 20: 'Hepatitis C', 21: 'Hepatitis D', 22: 'Hepatitis E', 3: 'Alcoholic hepatitis', 36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)', 18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism', 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal  Positional Vertigo', 2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
 
 
-description = pd.read_csv("C:\\Users\\sssnj\\OneDrive\\Desktop\\medical recomedation system\\description.csv")
-precautions = pd.read_csv("C:\\Users\\sssnj\\OneDrive\\Desktop\\medical recomedation system\\precautions_df.csv")
-workout = pd.read_csv("C:\\Users\\sssnj\\OneDrive\\Desktop\\medical recomedation system\\workout_df.csv")
-medications = pd.read_csv('C:\\Users\\sssnj\\OneDrive\\Desktop\\medical recomedation system\\medications.csv')
-diets = pd.read_csv("C:\\Users\\sssnj\\OneDrive\\Desktop\\medical recomedation system\\diets.csv")
+description = pd.read_csv("description.csv")
+precautions = pd.read_csv("precautions_df.csv")
+workout = pd.read_csv("workout_df.csv")
+medications = pd.read_csv('medications.csv')
+diets = pd.read_csv("diets.csv")
 
 # FastAPI app
 app = FastAPI()
